5c386a25f2f1b338ce41810d060d4204bc72aa10
edymartinez228 <martinezedy83@gmail.com>
2026-06-23 17:22:42 -0600

    Update title for predictive recycling model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from PIL import Image
 
 st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
-st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026-Joksan-Zavala")
+st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026-Edy-Martinez")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre entrenado")
 
 IMG_SIZE = (224, 224)
